8fc240357bc6e6f86cea9e523fcd062fe20cc6e5
Alen Nelson <alennelson2004@gmail.com>
2026-07-22 15:19:50 +0530

    Test CI/CD pipeline

--- backend/server.js
+++ backend/server.js
@@ -376,5 +376,5 @@ app.get("/api/test/db", async (req, res) => {
 /* ================= START SERVER ================= */
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server started on port ${PORT}`);
+  console.log(`🚀 Server started on port ${PORT} - CI/CD Test`);
 });
